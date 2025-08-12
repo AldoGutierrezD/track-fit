088ba201e255b7c445680ff98c479d47eb49d34e
AldoGutierrezD <aldogd24@gmail.com>
2025-08-12 16:23:32 -0600

    Se añadió el btn de macros del día

--- src/components/LeftBar.tsx
+++ src/components/LeftBar.tsx
@@ -1,8 +1,12 @@
+import ButtonMenu from "./ButtonMenu";
+import { Flame } from "lucide-react";
+
 export default function LeftBar() {
     return (
         <aside className="fixed w-24 h-screen p-4">
-            <div className="w-full h-full bg-foreground rounded-2xl">
-                <span>A</span>
+            <div className="w-full h-full flex flex-col justify-start items-center gap-2 bg-foreground rounded-2xl p-2">
+                <a href="/" className="font-against text-white text-3xl my-3">A</a>
+                <ButtonMenu link="#" title="Macros del día" icon={Flame} />
             </div>
         </aside>
     )
