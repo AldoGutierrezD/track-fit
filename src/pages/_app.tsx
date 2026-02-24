26d29b5ad57ea5de6a7be3bd631dcf28f63f1e51
AldoGutierrezD <aldogd24@gmail.com>
2026-02-24 16:36:27 -0600

    Se define un titulo de página y favicon temporal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>Track Fit</title>
+                <meta name="description" content="Sistema de control de dietas y entrenamiento" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
+
+            <Component {...pageProps} />
+        </>
+    );
 }
