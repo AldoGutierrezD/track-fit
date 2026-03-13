618ac19b14a29506a4c771f5f68cc2acfeae79ba
AldoGutierrezD <aldogd24@gmail.com>
2026-03-13 16:41:48 -0600

    [UPD] Se oculta momentaneamente la barra del lateral derecho

--- src/components/RightBar.tsx
+++ src/components/RightBar.tsx
@@ -4,7 +4,7 @@ export default function RightBar() {
     return (
         <aside className="hidden md:block w-80 h-screen sticky top-0 p-4 font-inter">
             <div className="relative w-full h-full rounded-2xl overflow-hidden">
-                <UserData />
+                {/* <UserData /> */}
             </div>
         </aside>
     )
